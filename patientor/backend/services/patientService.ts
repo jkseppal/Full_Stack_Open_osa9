6f4294872c63d2f344008c97c1f6938027360ea2
jkseppal <jani.k.seppala@gmail.com>
2020-09-28 01:00:50 +0300

    9.13 tehty

--- patientor/backend/services/patientService.ts
+++ patientor/backend/services/patientService.ts
@@ -1,6 +1,7 @@
 import patientData from '../data/patientsts';
-
-import { NonSsnPatients, Patient } from '../types';
+//import { v4 as uuidv4 } from 'uuid';
+import uuid = require('uuid');
+import { NonSsnPatients, Patient, NewPatient } from '../types';
 
 //const patients: Array<Patient> = patientData;
 
@@ -18,7 +19,18 @@ const getPatients = (): NonSsnPatients[] => {
   }));
 };
 
+const addPatient = ( entry: NewPatient ): Patient => {
+  const iidee: string = uuid.v4();
+  const newPatient = {
+    id: iidee,
+    ...entry
+  };
+  patientData.push(newPatient);
+  return newPatient;
+};
+
 export default {
   getPatients,
-  pat
+  pat,
+  addPatient
 };
--- patientor/backend/types.ts
+++ patientor/backend/types.ts
@@ -14,3 +14,11 @@ export interface Patient {
 }
 
 export type NonSsnPatients = Omit<Patient, 'ssn'>;
+
+export type NewPatient = Omit<Patient, 'id'>;
+
+export enum Gender {
+  Male = 'male',
+  Female = 'female',
+  Other = 'other',
+}
